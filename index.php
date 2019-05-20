622fdc2168cfffea2ad9f251d0ad3db0a5d34eee
Rafael Cavalcante <rafael.ifrn@gmail.com>
2019-05-20 21:42:13 +0100

    uploading...

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="public/library/node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="public/library/node_modules/bootswatch/dist/minty/bootstrap.min.css">
+        <link rel="stylesheet" href="public/library/node_modules/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+
+            <!-- Jumbotron -->
+            <div class="card card-inverse mb-5 header-container">
+                <img class="card-img" src="https://images.pexels.com/photos/1919236/pexels-photo-1919236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+                <div class="text-white text-center rgba-stylish-strong py-5 px-4 card-img-overlay" style="background: rgba(0, 0, 0, 0.3)">
+                    <div class="row justify-content-between">
+                        <div class="col-6 col-md-3">
+                            <img src="public/img/logo.png" alt="" style="margin-top: -60px; width: 100%;">
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <img src="public/img/google-play.png" alt="" class="google-play" style="width: 100%;">
+                        </div>
+                    </div>
+
+                    <div class="py-5">
+                
+                        <!-- Content -->
+                        <h2 class="card-title h2 my-4 py-2">Health & Records</h2>
+                        <p class="mb-4 pb-2 px-md-5 mx-md-5">If you are a new or existing care facility and want to facilitate doctors and nurses work, our Doctor on Call app can help you with that. Check out our application with a three months free trial and see if this fulfills your company needs . 
+
+                        </p>
+                        <a class="btn btn-outline-success" href="#" style="color: #fff; border-color: #fff"><i class="fas fa-clone left"></i> Learn more</a>
+                
+                    </div>
+                </div>
+            </div>
+            <!-- /Jumbotron -->
+
+            <div class="row my-5 text-center">
+                <div class="col-12">
+                    <h4>Doctor On Call app, provides you with easy to use and user friendly interface to search for Doctors available to call.</h4>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div> <!--/title -->
+
+            <div class="row justify-content-center">
+                <div class="col-11 col-md-8">
+
+                    <div class="media mb-5">
+                        <img src="public/img/mainpage.PNG" class="mr-3 app-screenshot" alt="appication main page application">
+                        <div class="media-body ml-3">
+                            <h5 class="mt-0">Services</h5>
+                            The main page application of the app consists of  three tasks, one is to call a doctor the second task  stores patients notes and the last task is where the doctor stores patients details once a patient was assessed  by the doctor.
+                        </div>
+                    </div>
+                
+                    <div class="media mb-5">
+                        <div class="media-body mr-3">
+                            <h5 class="mt-0 mb-1">Doctor available to call</h5>
+                            Doctors that are available to call are shown online, by clicking on the green button you agree to call a doctor to come to your services. 
+                        </div>
+                        <img src="public/img/doctorchoose.PNG" class="mr-3 app-screenshot" alt="doctor availabe to choose page application">
+                    </div>
+                
+                    <div class="media mb-5">
+                        <img src="public/img/patientdetail1.PNG" class="mr-3 app-screenshot" alt="patiens detail page application">
+                        <div class="media-body ml-3">
+                            <h5 class="mt-0">Patients details</h5>
+                            Information on patients medical history is stored on patients details, giving you easy access to input and output data on the patient's information list.
+                        </div>
+                    </div>
+
+                    <div class="media mb-5">  <!--image marging bottom -->
+                        <div class="media-body mr-3">
+                            <h5 class="mt-0 mb-1">Doctors notes</h5>
+                            Doctors notes give access to a doctor to write on patients consultation screen the diagnosis and the plan to follow after diagnosis
+                            </div>
+                        <img src="public/img/doconsulation1.PNG" class="mr-3 app-screenshot" alt="doctor notes on patiens page application">
+                    </div>
+
+                </div>
+            </div>    <!-- /<div class="row justify-content-center"> -->
+
+        </div>   <!-- /container -->
+
+        <footer>
+            <div class="row">
+                <div class="col-4">
+                    
+                </div>
+                <div class="col-4">
+                    
+                </div>
+                <div class="col-4">
+                    <h5>Company</h5>
+                </div>
+            </div>
+        </footer>
+        
+        
+        <script src="public/library/node_modules/jquery/dist/jquery.min.js" async defer></script>
+        <script src="public/library/node_modules/bootstrap/dist/js/bootstrap.min.js" async defer></script>
+        <script src="public/js/app.js" async defer></script>
+    </body>
+</html>
